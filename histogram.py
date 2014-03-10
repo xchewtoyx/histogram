f0fc6aee794ca6970311b13c1ae26145cc6f9ece
russell <russell@7c2e1d28-9bdd-0310-bedc-aab8470a6217>
2014-03-10 18:15:39 +0000

    Add a breakdown of the most common 80% versus the long tail
    git-svn-id: svn+ssh://svn.linx.net/linx/russell/scripts/ioping@3246 7c2e1d28-9bdd-0310-bedc-aab8470a6217

--- histogram.py
+++ histogram.py
@@ -6,26 +6,45 @@ import sys
 
 import numpy
 
-def main(args):
+def read_logfile(filename):
   readings = []
-  logger = logging.getLogger()
-  logger.setLevel(logging.INFO)
   try:
-    with open(args[0], 'r') as logfile:
+    with open(filename, 'r') as logfile:
       for line in logfile:
         time, reading = line.split(': ')
         readings.append([int(time), float(reading)])
   except IOError:
     logging.error('Unable to open file: args=%r', args)
+    raise
+  return readings
 
-  readings_array = numpy.array(readings)
-
-  (bins, edges) = numpy.histogram(readings_array[:,1], bins=20)
-
+def print_histogram(title, readings_array):
+  print title
+  print '=' * len(title)
+  (bins, edges) = numpy.histogram(readings_array, bins=10)
   scale = 50.0 / max(bins)
-
   for i in range(len(bins)):
     print '[%10.3f] [%10d] %s' % (edges[i], bins[i], '*' * int(bins[i]*scale))
+  print
+
+def main(args):
+  logger = logging.getLogger()
+  logger.setLevel(logging.INFO)
+
+  readings = read_logfile(args[0])
+  readings_array = numpy.array(readings)
+
+  print_histogram('Full distribution', readings_array[:,1])
+
+  eighty_twenty = numpy.percentile(readings_array[:,1], 80)
+
+  print_histogram(
+    'Distribution of 80th percentile',
+    [value for value in readings_array[:,1] if value <= eighty_twenty])
+
+  print_histogram(
+    'Distribution of remainder',
+    [value for value in readings_array[:,1] if value > eighty_twenty])
 
 if __name__ == '__main__':
   main(sys.argv[1:])
