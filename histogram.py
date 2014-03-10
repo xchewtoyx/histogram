b13cb9b81f024d187d5408eaa2fbf58088608ad7
russell <russell@7c2e1d28-9bdd-0310-bedc-aab8470a6217>
2014-03-10 18:15:39 +0000

    Add some extra histograms and a summary section with information about the distribution.
    git-svn-id: svn+ssh://svn.linx.net/linx/russell/scripts/ioping@3247 7c2e1d28-9bdd-0310-bedc-aab8470a6217

--- histogram.py
+++ histogram.py
@@ -18,9 +18,20 @@ def read_logfile(filename):
     raise
   return readings
 
-def print_histogram(title, readings_array):
+def print_title(title):
   print title
   print '=' * len(title)
+
+def print_distribution_stats(readings_array):
+  print 'Minimum: %0.3f' % readings_array.min()
+  print 'Maximum: %0.3f' % readings_array.max()
+  print 'Mean: %0.3f' % readings_array.mean()
+  print 'Deviation: %0.3f' % readings_array.std()
+  print 'Median: %0.3f' % numpy.percentile(readings_array, 50)
+  print '80th Percentile: %0.3f' % numpy.percentile(readings_array, 80)
+  print
+
+def print_histogram(readings_array):
   (bins, edges) = numpy.histogram(readings_array, bins=10)
   scale = 50.0 / max(bins)
   for i in range(len(bins)):
@@ -32,19 +43,23 @@ def main(args):
   logger.setLevel(logging.INFO)
 
   readings = read_logfile(args[0])
-  readings_array = numpy.array(readings)
+  readings_array = numpy.array(readings)[:,1]
 
-  print_histogram('Full distribution', readings_array[:,1])
+  print_title('Distribution details')
+  print_distribution_stats(readings_array)
 
-  eighty_twenty = numpy.percentile(readings_array[:,1], 80)
+  print_title('Full distribution histogram')
+  print_histogram(readings_array)
 
+  eighty_twenty = numpy.percentile(readings_array, 80)
+
+  print_title('Distribution of 80th percentile')
   print_histogram(
-    'Distribution of 80th percentile',
-    [value for value in readings_array[:,1] if value <= eighty_twenty])
+    [value for value in readings_array if value <= eighty_twenty])
 
+  print_title('Distribution of remainder')
   print_histogram(
-    'Distribution of remainder',
-    [value for value in readings_array[:,1] if value > eighty_twenty])
+    [value for value in readings_array if value > eighty_twenty])
 
 if __name__ == '__main__':
   main(sys.argv[1:])
